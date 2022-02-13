76d63ec43f2865f7e0b9bacd39fea127d8e6d3d2
Seth Feaganes <sfeaganes@gmail.com>
2022-02-13 16:48:40 -0500

    add led_blink and led_off Raspberry Pi code

---
+++ led_blink.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(37, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(33, GPIO.OUT, initial=GPIO.HIGH)
+
+while True:
+    GPIO.output(37, GPIO.HIGH)
+    GPIO.output(33, GPIO.LOW)
+    sleep(.3)
+    GPIO.output(37, GPIO.LOW)
+    GPIO.output(33, GPIO.HIGH)
+    sleep(.3)
+
---
+++ led_off.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(37, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(33, GPIO.OUT, initial=GPIO.LOW)
+
+
+
+
+
+
+
+
+
