61cae8799e26032a54b7f32e320b59c8a45d16a9
Seth Feaganes <sfeaganes@gmail.com>
2022-02-15 17:12:10 -0500

    Add relay_on.py

---
+++ relay_on.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(36, GPIO.OUT, initial=GPIO.LOW)
+
+GPIO.output(36, GPIO.HIGH)
+sleep(3)
+GPIO.output(36, GPIO.LOW)
+
+
+
+
+
+
+
+
+
