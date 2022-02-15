ef904f8801efb9562dd2de483ea55743e19ef816
Seth Feaganes <sfeaganes@gmail.com>
2022-02-15 18:40:34 -0500

    Add changes to turn on/off LEDs

--- tmp_relay_test.py
+++ tmp_relay_test.py
@@ -10,6 +10,8 @@ from time import sleep
 GPIO.setwarnings(False)
 #GPIO.setmode(GPIO.BOARD)
 GPIO.setup(16, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(13, GPIO.OUT, initial=GPIO.LOW)  # Red LED
+GPIO.setup(26, GPIO.OUT, initial=GPIO.HIGH) # Green LED
 
 # Create sensor object to read temp and humidity from SHT30
 i2c = board.I2C()
@@ -22,10 +24,14 @@ while True:
     print("Humidity: %0.1f %%" % sensor.relative_humidity)
     if relay_status == 0 and sensor.temperature > 32.222:
         GPIO.output(16, GPIO.HIGH)
+        GPIO.output(26, GPIO.LOW)
+        GPIO.output(13, GPIO.HIGH)
         relay_status = 1
         print("Cooling activated")
     elif relay_status == 1 and sensor.temperature < 32.222:
         GPIO.output(16, GPIO.LOW)
+        GPIO.output(13, GPIO.LOW)
+        GPIO.output(26, GPIO.HIGH)
         relay_status = 0
         print("Cooling Deactivated") 
     else:
