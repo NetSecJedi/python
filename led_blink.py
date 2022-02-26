037feb4e997a134d0e4448361bd7a01a283217fd
Seth Feaganes <sfeaganes@gmail.com>
2022-02-26 11:38:51 -0500

    re-add led_blink.py

--- led_blink.py
+++ led_blink.py
@@ -4,12 +4,12 @@ from time import sleep
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(16, GPIO.OUT, initial=GPIO.LOW)
-#GPIO.setup(33, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(33, GPIO.OUT, initial=GPIO.HIGH)
 
 while True:
     GPIO.output(16, GPIO.HIGH)
-    #GPIO.output(33, GPIO.LOW)
+    GPIO.output(33, GPIO.LOW)
     sleep(.3)
     GPIO.output(16, GPIO.LOW)
-    #GPIO.output(33, GPIO.HIGH)
+    GPIO.output(33, GPIO.HIGH)
     sleep(.3)
