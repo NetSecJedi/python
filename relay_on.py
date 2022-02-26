1a4120d7211ab27fec47d38683ff2838f8aee54f
Seth Feaganes <sfeaganes@gmail.com>
2022-02-25 19:08:32 -0500

    relay_on mods

--- relay_on.py
+++ relay_on.py
@@ -3,8 +3,14 @@ from time import sleep
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(36, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(36, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(38, GPIO.OUT, initial=GPIO.HIGH)
 
-GPIO.output(36, GPIO.HIGH)
-sleep(3)
+sleep(5)
 GPIO.output(36, GPIO.LOW)
+sleep(5)
+GPIO.output(36, GPIO.HIGH)
+sleep(5)
+GPIO.output(38, GPIO.LOW)
+sleep(5)
+GPIO.output(38, GPIO.HIGH)
