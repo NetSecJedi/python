1f488e1d54c7b72bc9e43d59aee183eca8d03bff
Seth Feaganes <sfeaganes@gmail.com>
2022-02-26 11:29:06 -0500

    led testing

--- led_blink.py
+++ led_blink.py
@@ -3,13 +3,13 @@ from time import sleep
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(37, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(33, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(16, GPIO.OUT, initial=GPIO.LOW)
+#GPIO.setup(33, GPIO.OUT, initial=GPIO.HIGH)
 
 while True:
-    GPIO.output(37, GPIO.HIGH)
-    GPIO.output(33, GPIO.LOW)
+    GPIO.output(16, GPIO.HIGH)
+    #GPIO.output(33, GPIO.LOW)
     sleep(.3)
-    GPIO.output(37, GPIO.LOW)
-    GPIO.output(33, GPIO.HIGH)
+    GPIO.output(16, GPIO.LOW)
+    #GPIO.output(33, GPIO.HIGH)
     sleep(.3)
