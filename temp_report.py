4998c9835ceac1870369b00298766bc832e7ff3c
Seth Feaganes <sfeaganes@gmail.com>
2022-03-05 09:29:05 -0500

    add error logging

--- temp_report.py
+++ temp_report.py
@@ -3,8 +3,11 @@ import adafruit_sht31d
 from time import sleep
 import logging
 
-# Set up log file
+# Set up INFO and DEBUG log files
 logging.basicConfig(filename='/home/pi/temp_hum.log', encoding='utf-8', format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
+err_logger = logging.getLogger()
+err_logger.basicConfig(filename='/home/pi/temp_hum_err.log', encoding='utf-8', format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
+
 
 # Create sensor object to read temp and humidity from SHT30
 i2c = board.I2C()
@@ -14,7 +17,7 @@ try:
     temp = sensor.temperature # read temperature 
     humidity = sensor.relative_humidity # read humidity
 except:
-    logging.error("Error reading sensor, retrying....")
+    err_logger.error("Error reading sensor, in use")
 
 logging.info("Temperature: %0.1f C" % temp)
 logging.info("Humidity: %0.1f %%" % humidity)
