658f9e088abc558764b4ca8d410cfdd906b565a4
Seth Feaganes <sfeaganes@gmail.com>
2022-07-17 17:25:52 -0400

    .

--- logger.py
+++ logger.py
@@ -1,9 +1,11 @@
 #import logging
-import write_logs
 import syslog
+
+def write_syslog(facility = syslog.LOG_INFO, msg = "Message not specified"):
+    syslog.syslog(facility, msg)
 
 #logging.basicConfig(filename='fan.log', encoding='utf-8', level=logging.DEBUG)
 #logging.info('test INFO message')
 #logging.warning('test WARNING message')
 
-write_logs.write_syslog(syslog.LOG_ERR, "This is a test message....")
+write_syslog(syslog.LOG_ERR, "This is a test message....")
