2911c35e5c69e4f8bec2498caae437af391fc3c0
Seth Feaganes <sfeaganes@gmail.com>
2024-11-16 21:38:26 -0500

    add while statement

--- prometheus_test.py
+++ prometheus_test.py
@@ -1,6 +1,9 @@
 from prometheus_client import Gauge, start_http_server
 import random
-
+import time 
 g = Gauge('python_test_guage', 'A test guage from Python code')
 start_http_server(8000)
-g.set(random.random() * 15 - 5)
+
+while True:
+    g.set(random.random() * 15 - 5)
+    time.sleep(5)
