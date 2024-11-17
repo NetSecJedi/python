5605feaa5f2367e3f87c76e808d3def143e94800
Seth Feaganes <sfeaganes@gmail.com>
2024-11-16 21:31:41 -0500

    correct misspelling of Gauge

--- prometheus_test.py
+++ prometheus_test.py
@@ -1,6 +1,6 @@
-from prometheus_client import Guage, start_http_server
+from prometheus_client import Gauge, start_http_server
 import random
 
-g = Guage('python_test_guage', 'A test guage from Python code')
+g = Gauge('python_test_guage', 'A test guage from Python code')
 start_http_server(8000)
 g.Set(random.random() * 15 - 5)
