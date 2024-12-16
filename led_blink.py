a7b2afa4829065252b15fc9d5dd7990ae0a0e290
Seth Feaganes <sfeaganes@gmail.com>
2024-12-15 20:59:34 -0500

    Add Blue LED to blink script

--- led_blink.py
+++ led_blink.py
@@ -4,12 +4,15 @@ import board
 
 GPIO.setwarnings(False)
 GPIO.setup(25, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(24, GPIO.OUT, initial=GPIO.LOW)
 
 def read_err_wait():
     for x in range(30):
         GPIO.output(25, GPIO.LOW)
+        GPIO.output(24, GPIO.HIGH)
         sleep(0.5)
         GPIO.output(25, GPIO.HIGH)
+        GPIO.output(24, GPIO.LOW)
         sleep(0.5)
 
 read_err_wait()
