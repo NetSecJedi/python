18af768f61fec83a4e269111aba4157e39f468d9
Seth Feaganes <sfeaganes@gmail.com>
2024-12-15 20:54:31 -0500

    Use correct pin numbers

--- led_off.py
+++ led_off.py
@@ -3,9 +3,9 @@ from time import sleep
 import board
 
 GPIO.setwarnings(False)
-GPIO.setup(37, GPIO.OUT, initial=GPIO.HIGH)
-GPIO.setup(33, GPIO.OUT, initial=GPIO.HIGH)
+#GPIO.setup(37, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(24, GPIO.OUT, initial=GPIO.HIGH)
 GPIO.setup(25, GPIO.OUT, initial=GPIO.HIGH)
 #GPIO.output(37, GPIO.LOW)
-#GPIO.output(33, GPIO.LOW)
-#GPIO.output(25, GPIO.LOW)
+GPIO.output(24, GPIO.LOW)
+GPIO.output(25, GPIO.LOW)
